2680fd884028ac0ef8ab32a3ee66d0a6582c003f
Eugene <eugene97@connect.hku.hk>
2023-11-07 12:57:47 +0800

    test: check invalid data post to newsletters endpoint

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,18 @@
+use actix_web::web;
 use actix_web::HttpResponse;
 
-pub async fn publish_newsletter() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
+
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
