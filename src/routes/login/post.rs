8371f24f33dee41d8c8f46ede5c4bf2859907a38
Eugene <eugene97@connect.hku.hk>
2023-11-24 08:50:49 +0800

    feat: show login error message by query parameters

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,6 @@
 use crate::authentication::{validate_credentials, AuthError, Credentials};
 use crate::routes::error_chain_fmt;
-use actix_web::http::header::{ContentType, LOCATION};
+use actix_web::http::header::LOCATION;
 use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
 use actix_web::{web, ResponseError};
@@ -29,44 +29,14 @@ impl std::fmt::Debug for LoginError {
 
 impl ResponseError for LoginError {
     fn status_code(&self) -> StatusCode {
-        match self {
-            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
-        }
+        StatusCode::SEE_OTHER
     }
 
     fn error_response(&self) -> HttpResponse {
+        let encoded_error = urlencoding::Encoded::new(self.to_string());
         HttpResponse::build(self.status_code())
-            .content_type(ContentType::html())
-            .body(format!(
-                r#"<!DOCTYPE html>
-            <head>
-                <meta http-equiv="content-type" content="text/html"; charset=utf-8>
-                <title>Login</title>
-            </head>
-            <body>
-                <p><i>{}</i></p>
-                <form action="/login" method="post">
-                    <label>Username
-                        <input
-                            type="text"
-                            placeholder="Enter Username"
-                            name="username"
-                        >
-                    </label>
-                    <label>Password
-                        <input
-                            type="password"
-                            placeholder="Enter Password"
-                            name="password"
-                        >
-                    </label>
-                    <button type="submit">Login</button>
-                </form>
-            </body>
-            </html>"#,
-                self
-            ))
+            .insert_header((LOCATION, format!("/login?error={}", encoded_error)))
+            .finish()
     }
 }
 
