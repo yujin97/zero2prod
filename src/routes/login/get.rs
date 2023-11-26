493bcca7280e38c4b720fb03e8a85419f5f71029
Eugene <eugene97@connect.hku.hk>
2023-11-26 09:42:21 +0800

    feat: implement verify method on QueryParams

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,4 +1,6 @@
 use actix_web::{http::header::ContentType, web, HttpResponse};
+use hmac::{Hmac, Mac};
+use secrecy::ExposeSecret;
 
 use crate::startup::HmacSecret;
 
@@ -8,15 +10,39 @@ pub struct QueryParams {
     tag: String,
 }
 
+impl QueryParams {
+    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
+        let tag = hex::decode(self.tag)?;
+        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
+
+        let mut mac =
+            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
+        mac.update(query_string.as_bytes());
+        mac.verify_slice(&tag)?;
+
+        Ok(self.error)
+    }
+}
+
 pub async fn login_form(
-    query: web::Query<Option<QueryParams>>,
+    query: Option<web::Query<QueryParams>>,
     secret: web::Data<HmacSecret>,
 ) -> HttpResponse {
-    let error_html = match query.0 {
+    let error_html = match query {
         None => "".into(),
-        Some(query) => {
-            format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&query.error))
-        }
+        Some(query) => match query.0.verify(&secret) {
+            Ok(error) => {
+                format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error))
+            }
+            Err(e) => {
+                tracing::warn!(
+                    error.message = %e,
+                    error.cause_chain = ?e,
+                    "Failed to verify query parameters using the HMAC tag"
+                );
+                "".into()
+            }
+        },
     };
     HttpResponse::Ok()
         .content_type(ContentType::html())
