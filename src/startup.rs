93bcdfa3127fc95ca4a43103af24740f2d45989b
Eugene <eugene97@connect.hku.hk>
2023-12-08 08:58:19 +0800

    feat: handle login with session

--- src/startup.rs
+++ src/startup.rs
@@ -2,7 +2,7 @@ use crate::configuration::{DatabaseSettings, Settings};
 
 use crate::email_client::EmailClient;
 use crate::routes::{
-    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+    admin_dashboard, confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
 use actix_session::storage::RedisSessionStore;
 use actix_session::SessionMiddleware;
@@ -108,6 +108,7 @@ pub async fn run(
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
             .route("/login", web::post().to(login))
+            .route("/admin/dashboard", web::get().to(admin_dashboard))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
