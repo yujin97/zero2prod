716fba635ad048f976d4c90052331bfeef4acd6c
Eugene <eugene97@connect.hku.hk>
2023-12-11 08:20:40 +0800

    chore: add migration for seed user

---
+++ migrations/20231211000928_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'be9cf379-8de1-4269-a0b2-2c3a8a1e9247',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$2PLOIcBHoVxLELD1dzV/Tw$oiUD9UwBd1bjHdZL1KDSiscgZXBv97MnGQLeQOIocKI'
+);
