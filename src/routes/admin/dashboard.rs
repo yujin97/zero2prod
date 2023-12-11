8fbfb535bb34c3063e311fd246b20075890524d2
Eugene <eugene97@connect.hku.hk>
2023-12-11 22:06:45 +0800

    feat: add change password page

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,17 +1,11 @@
 use crate::session_state::TypedSession;
+use crate::utils::e500;
 use actix_web::http::header::ContentType;
 use actix_web::http::header::LOCATION;
 use actix_web::{web, HttpResponse};
 use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
-
-fn e500<T>(e: T) -> actix_web::Error
-where
-    T: std::fmt::Debug + std::fmt::Display + 'static,
-{
-    actix_web::error::ErrorInternalServerError(e)
-}
 
 pub async fn admin_dashboard(
     session: TypedSession,
@@ -28,12 +22,17 @@ pub async fn admin_dashboard(
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
+                <html lang="en">
                 <head>
                     <meta http-equiv="content-type" content="text/html"; charset=utf-8>
                     <title>Admin dashboard</title>
                 </head>
                 <body>
                     <p>Welcome {username}</p>
+                    <p>Available actions:</p>
+                    <ol>
+                        <li><a href="/admin/password">Change Password</a></li>
+                    </ol>
                 </body>
             </html>"#
         )))
