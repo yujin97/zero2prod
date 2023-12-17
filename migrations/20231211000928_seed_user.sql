24b9c497bb5b401ead22412184359a6e019a9619
Eugene <eugene97@connect.hku.hk>
2023-12-17 22:16:45 +0800

    chore: edit seed user credentials

--- migrations/20231211000928_seed_user.sql
+++ migrations/20231211000928_seed_user.sql
@@ -2,5 +2,5 @@ INSERT INTO users (user_id, username, password_hash)
 VALUES (
     'be9cf379-8de1-4269-a0b2-2c3a8a1e9247',
     'admin',
-    '$argon2id$v=19$m=15000,t=2,p=1$2PLOIcBHoVxLELD1dzV/Tw$oiUD9UwBd1bjHdZL1KDSiscgZXBv97MnGQLeQOIocKI'
+    '$argon2id$v=19$m=15000,t=2,p=1$3W8ve90udH+IlzZCmSOMiA$nBc6ITLsTDHF1L8hKwI8lm1p3je1IG5G7NS+/yBxZKQ'
 );
