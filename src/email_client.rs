cfaf9037bea61e8704698189075f6581650af3dd
Eugene <eugene97@connect.hku.hk>
2023-12-22 23:28:33 +0800

    refactor: move newsletter routes under admin routes

--- src/email_client.rs
+++ src/email_client.rs
@@ -109,7 +109,6 @@ mod tests {
         fn matches(&self, request: &Request) -> bool {
             let result: Result<serde_json::Value, _> = serde_json::from_slice(&request.body);
             if let Ok(body) = result {
-                dbg!(&body);
                 body.get("To").is_some()
                     && body.get("Subject").is_some()
                     && body.get("HtmlBody").is_some()
--- src/startup.rs
+++ src/startup.rs
@@ -4,7 +4,7 @@ use crate::authentication::reject_anonymous_users;
 use crate::email_client::EmailClient;
 use crate::routes::{
     admin_dashboard, change_password, change_password_form, confirm, health_check, home, log_out,
-    login, login_form, newsletter_dashboard, publish_newsletter, subscribe,
+    login, login_form, publish_newsletter, publish_newsletter_form, subscribe,
 };
 use actix_session::storage::RedisSessionStore;
 use actix_session::SessionMiddleware;
@@ -117,7 +117,7 @@ pub async fn run(
                     .route("/password", web::get().to(change_password_form))
                     .route("/password", web::post().to(change_password))
                     .route("/logout", web::post().to(log_out))
-                    .route("/newsletters", web::get().to(newsletter_dashboard))
+                    .route("/newsletters", web::get().to(publish_newsletter_form))
                     .route("/newsletters", web::post().to(publish_newsletter)),
             )
             .app_data(db_pool.clone())
