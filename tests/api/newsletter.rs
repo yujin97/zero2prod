5c4e06dc67e41766223245b1cf201ae920d450a9
Eugene <eugene97@connect.hku.hk>
2023-12-25 23:43:45 +0800

    test: check that newsletter creation is idempotent

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -27,7 +27,7 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
         "html_content": "<p>Newsletter body as HTML</p>",
     });
 
-    let response = app.post_newsletters(&newsletter_request_body).await;
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
     assert_eq!(response.status().as_u16(), 303);
 }
 
@@ -55,7 +55,7 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
     }))
     .await;
 
-    let response = app.post_newsletters(&newsletter_request_body).await;
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
     assert_eq!(response.status().as_u16(), 303);
 }
 
@@ -85,7 +85,7 @@ async fn newsletter_returns_400_for_invalid_data() {
     .await;
 
     for (invalid_body, error_message) in test_cases {
-        let response = app.post_newsletters(&invalid_body).await;
+        let response = app.post_publish_newsletter(&invalid_body).await;
         assert_eq!(
             400,
             response.status().as_u16(),
@@ -154,6 +154,39 @@ async fn invalid_password_is_rejected() {
         .expect("Failed to execute request.");
 
     assert_is_redirect_to(&response, "/login")
+}
+
+#[tokio::test]
+async fn newsletter_creation_is_idempotent() {
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string()
+    });
+
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    let html_page = app.get_publish_newsletter_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    let html_page = app.get_publish_newsletter_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
 }
 
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
